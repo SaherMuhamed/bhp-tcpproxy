ed5dbaf991075b5d6632d319cbb2567fe0af43e3
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2024-09-08 23:24:06 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import socket
 import threading
@@ -6,32 +8,31 @@ HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(
 
 
 def hexdump(src, length=16, show=True):
-    """helper function to display the communication between local and remote machines"""
     if isinstance(src, bytes):
         src = src.decode()
 
     results = list()
     for i in range(0, len(src), length):
         word = str(src[i:i + length])
+
         printable = word.translate(HEX_FILTER)
-        hexa = ' '.join([f'{ord(c):02x}' for c in word])
+        hexa = ' '.join([f'{ord(c):02X}' for c in word])
         hex_width = length * 3
         results.append(f'{i:04x}  {hexa:<{hex_width}}  {printable}')
 
-        if show:
-            for line in results:
-                print(line)
-            else:
-                return results
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
 
 
-def receive_from(connection):
-    """function that proxy will use to receive data from local and remote machines"""
+def receive_from(connection, buffer_size=4096, timeout=7):
     buffer = b""
-    connection.settimeout(7)
+    connection.settimeout(timeout)
     try:
         while True:
-            data = connection.recv(4096)
+            data = connection.recv(buffer_size)
             if not data:
                 break
             buffer += data
@@ -48,7 +49,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         remote_buffer = receive_from(connection=remote_socket)
         hexdump(src=remote_buffer)
 
-    remote_buffer = response_handler(buffer=remote_buffer)
+    # remote_buffer = response_handler(buffer=remote_buffer)
     if len(remote_buffer):
         print("[<==] Sending %d bytes to localhost" % len(remote_buffer))
         client_socket.send(remote_buffer)
@@ -56,11 +57,10 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     while True:
         local_buffer = receive_from(connection=client_socket)
         if len(local_buffer):
-            line = "[==>] Received %d bytes to localhost" % len(local_buffer)
-            print(line)
+            print("[==>] Received %d bytes to localhost" % len(local_buffer))
             hexdump(src=local_buffer)
 
-            local_buffer = request_handler(local_buffer)
+            # local_buffer = request_handler(local_buffer)
             remote_socket.send(local_buffer)
             print("[==>] Sent to remote")
 
@@ -69,59 +69,21 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print("[<==] Received %d bytes from remote" % len(remote_buffer))
             hexdump(src=remote_buffer)
 
-            remote_buffer = request_handler(remote_buffer)
+            # remote_buffer = request_handler(remote_buffer)
             client_socket.send(remote_buffer)
-            print("[<==] Sent to local")
+            print("[<==] Sent to localhost")
 
-        if not len(local_buffer) or not len(remote_buffer):
-            client_socket.close()
-            remote_socket.close()
-            print("[*] No more data. Closing connections")
-            break
-
-
-def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
-    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    try:
-        server.bind((local_host, local_port))  # open tcp server on our local machine (kali)
-    except Exception as e:
-        print("[-] Problem on bind: %r" % e)
-        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
-        print("[!!] Check for other listening sockets or correct permissions")
-        sys.exit(0)
-
-    print("\n[*] listening on %s:%d" % (local_host, local_port))
-    server.listen(7)
-
-    try:
-        while True:
-            client_socket, addr = server.accept()
-            print(">> Received incoming connection from %s:%d" % (addr[0], addr[1]))  # printing out local connection
-            # info
-            proxy_thread = threading.Thread(
-                target=proxy_handler,
-                args=(client_socket, remote_host, remote_port, receive_first)
-            )
-            proxy_thread.start()  # start thread to talk to the remote host
-    except KeyboardInterrupt:
-        print("\n[*] Detected 'ctrl + c' pressed, program terminated.")
-        sys.exit(0)
-
-
-def request_handler(buffer):
-    # perform packet modifications
-    return buffer
-
-
-def response_handler(buffer):
-    # perform packet modifications
-    return buffer
+        # if not len(local_buffer) or not len(remote_buffer):
+        #     client_socket.close()
+        #     remote_socket.close()
+        #     print("[*] No more data. Closing connections")
+        #     break
 
 
 def main():
     if len(sys.argv[1:]) != 5:
         print("Usage: python3 proxy.py [localhost] [localport] [remotehost] [remoteport] [receive_first]")
-        print("Example: python3 proxy.py 127.0.0.1 7000 10.7.78.1 8000 True")
+        print("Example: python3 proxy.py 192.168.1.11 21 10.7.78.1 21 True")
         sys.exit(0)
 
     local_host = sys.argv[1]
@@ -142,6 +104,44 @@ def main():
         remote_port=remote_port,
         receive_first=receive_first
     )
+
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))  # open tcp server on our local machine (kali)
+    except Exception as e:
+        print("problem on bind: %r" % e)
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions")
+        sys.exit(0)
+
+    print("\n[*] listening on %s:%d" % (local_host, local_port))
+    server.listen(7)
+
+    try:
+        while True:
+            client_socket, addr = server.accept()
+            print(">> Received incoming connection from %s:%d" % (addr[0], addr[1]))  # printing out local connection
+
+            proxy_thread = threading.Thread(
+                target=proxy_handler,
+                args=(client_socket, remote_host, remote_port, receive_first)
+            )
+            proxy_thread.start()  # start thread to talk to the remote host
+    except KeyboardInterrupt:
+        print("\n[*] Detected 'ctrl + c' pressed, program terminated.")
+        sys.exit(0)
+
+
+def request_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+
+def response_handler(buffer):
+    # perform packet modifications
+    return buffer
 
 
 if __name__ == "__main__":
