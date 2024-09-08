cbf340d675b9869c187fd621925aa7d307e20c7b
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2024-09-08 14:02:03 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -5,53 +5,28 @@ import threading
 HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
 
-def main():
-    if len(sys.argv[1:]) != 5:
-        print("Usage: ./proxy.py [localhost] [localport]", end='')
-        print("[remotehost] [remoteport] [receive_first]")
-        print("Example: ./proxy.py 127.0.0.1 7000 10.7.78.1 8000 True")
-        sys.exit(0)
-
-    local_host = sys.argv[1]
-    local_port = int(sys.argv[2])
-    remote_host = sys.argv[3]
-    remote_port = int(sys.argv[4])
-    receive_first = sys.argv[5]
-
-    if "True" in receive_first:
-        receive_first = True
-    else:
-        receive_first = False
-
-    server_loop(
-        local_host=local_host,
-        local_port=local_port,
-        remote_host=remote_host,
-        remote_port=remote_port,
-        receive_first=receive_first
-    )
-
-
 def hexdump(src, length=16, show=True):
+    """helper function to display the communication between local and remote machines"""
     if isinstance(src, bytes):
         src = src.decode()
+
     results = list()
     for i in range(0, len(src), length):
         word = str(src[i:i + length])
-
         printable = word.translate(HEX_FILTER)
         hexa = ' '.join([f'{ord(c):02x}' for c in word])
         hex_width = length * 3
         results.append(f'{i:04x}  {hexa:<{hex_width}}  {printable}')
 
-    if show:
-        for line in results:
-            print(line)
-    else:
-        return results
+        if show:
+            for line in results:
+                print(line)
+            else:
+                return results
 
 
 def receive_from(connection):
+    """function that proxy will use to receive data from local and remote machines"""
     buffer = b""
     connection.settimeout(7)
     try:
@@ -108,26 +83,29 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
     server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     try:
-        server.bind((local_host, local_port))
+        server.bind((local_host, local_port))  # open tcp server on our local machine (kali)
     except Exception as e:
-        print("problem on bind: %r" % e)
-
+        print("[-] Problem on bind: %r" % e)
         print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
         print("[!!] Check for other listening sockets or correct permissions")
         sys.exit(0)
 
-    print("[*] listening on %s:%d" % (local_host, local_port))
+    print("\n[*] listening on %s:%d" % (local_host, local_port))
     server.listen(7)
-    while True:
-        client_socket, addr = server.accept()
-        print("> Received incoming connection from %s:%d" % (addr[0], addr[1]))
 
-        # start thread to talk to the remote host
-        proxy_thread = threading.Thread(
-            target=proxy_handler,
-            args=(client_socket, remote_host, remote_port, receive_first)
-        )
-        proxy_thread.start()
+    try:
+        while True:
+            client_socket, addr = server.accept()
+            print(">> Received incoming connection from %s:%d" % (addr[0], addr[1]))  # printing out local connection
+            # info
+            proxy_thread = threading.Thread(
+                target=proxy_handler,
+                args=(client_socket, remote_host, remote_port, receive_first)
+            )
+            proxy_thread.start()  # start thread to talk to the remote host
+    except KeyboardInterrupt:
+        print("\n[*] Detected 'ctrl + c' pressed, program terminated.")
+        sys.exit(0)
 
 
 def request_handler(buffer):
@@ -138,6 +116,32 @@ def request_handler(buffer):
 def response_handler(buffer):
     # perform packet modifications
     return buffer
+
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: python3 proxy.py [localhost] [localport] [remotehost] [remoteport] [receive_first]")
+        print("Example: python3 proxy.py 127.0.0.1 7000 10.7.78.1 8000 True")
+        sys.exit(0)
+
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+
+    server_loop(
+        local_host=local_host,
+        local_port=local_port,
+        remote_host=remote_host,
+        remote_port=remote_port,
+        receive_first=receive_first
+    )
 
 
 if __name__ == "__main__":
